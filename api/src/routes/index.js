913d27b31b94f97e4e41e79dc32f236fc9c16b86
tomasld13 <tomasld13@gmail.com>
2022-06-24 19:39:40 -0300

    Se agregan rutas de Ciudades y Provincias. GET /api/provincias Trae todas las provincias y sus respectivas ciudades. GET /api/provincias/:id Trae la provincia que coincida con el id y sus respectivas ciudades. GET /api/ciudades Trae todas las ciudades con sus respectivas provincias. GET /api/ciudades/:id Trae la ciudad que coincida con el id y su respectiva provincia.

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,12 @@
 const Router = require("express")
 const psicologoRoutes = require("./PsicologoController/psicologoRoutes")
 const pacienteRoutes = require("./PacienteController/pacienteRoutes")
-const ciudadesRoutes = require("./Ciudades/ciudadesRoutes")
+const ciudadesRoutes = require("./CiudadesController/ciudadesRoutes")
+const provinciasRoutes = require("./ProvinciasController/provinciaRoutes")
 const router = Router();
 
 router.use('/psicologo', psicologoRoutes);
 router.use('/paciente', pacienteRoutes);
 router.use('/ciudades', ciudadesRoutes);
+router.use('/provincias', provinciasRoutes)
 module.exports = router;
