68bc4f128f3709db2b3132b95aa7838ef0105016
tomasld13 <tomasld13@gmail.com>
2022-06-24 20:03:51 -0300

    Redireccionamiento de petición Localidades Se cambia de petición y en vez de traer los municipios se traen las locales ya que con la division politica de municipios algunas provincias quedaban sin ciudades.

--- api/src/creadores/ciudades.js
+++ api/src/creadores/ciudades.js
@@ -2,8 +2,8 @@ const { Ciudad, Provincia } = require("../db")
 const axios = require("axios")
 
 const getCiudades = async () => {
-    const ciudades = await axios.get("https://infra.datos.gob.ar/catalog/modernizacion/dataset/7/distribution/7.4/download/municipios.json")
-    await ciudades.data.municipios.map(m => async function(){
+    const ciudades = await axios.get("https://infra.datos.gob.ar/catalog/modernizacion/dataset/7/distribution/7.5/download/localidades.json")
+    await ciudades.data.localidades.map(m => async function(){
         const ciudadActual = await Ciudad.create({name: m.nombre})
         const provincia = await Provincia.findByPk(m.provincia.id)
         await ciudadActual.setProvincium(provincia)
