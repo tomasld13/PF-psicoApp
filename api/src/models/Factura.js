67b90f66bc89db7dc90f42dc0d1bd63f798d9dff
tomasld13 <tomasld13@gmail.com>
2022-06-23 22:51:21 -0300

    Se agregaron generadores. Generadores de: Provincias-Ciudades-Usuarios. Se modifico modelo Provincia en id. Se crea rutas de Ciudades para su get.

--- api/src/models/Factura.js
+++ api/src/models/Factura.js
@@ -3,6 +3,9 @@ const {DataTypes} = require('sequelize');
 
 module.exports = (sequelize)=>{
     sequelize.define('factura',{
+        precio: {
+            type: DataTypes.INTEGER
+        }
     },
     {
         timestamps : false
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,10 @@
 const Router = require("express")
 const psicologoRoutes = require("./PsicologoController/psicologoRoutes")
 const pacienteRoutes = require("./PacienteController/pacienteRoutes")
+const ciudadesRoutes = require("./Ciudades/ciudadesRoutes")
 const router = Router();
 
 router.use('/psicologo', psicologoRoutes);
 router.use('/paciente', pacienteRoutes);
-
+router.use('/ciudades', ciudadesRoutes);
 module.exports = router;
