42fab78e7f97be82fcd06bcdd49716351c1a4cf6
tomasld13 <tomasld13@gmail.com>
2022-06-24 21:11:57 -0300

    Se crean rutas para pedir psicologos de ciertas provincias o ciudades. GET /api/psicologo/provincia/:provincia Mandando el nombre de una provincia por params se traen los psicologos que esten asociados a esta misma. Get /api/psicologo/ciudad/:ciudad Mandando el nombre de una ciudad por params se traen los psicologos que esten asociados a esta.

--- api/src/creadores/ciudades.js
+++ api/src/creadores/ciudades.js
@@ -3,6 +3,7 @@ const axios = require("axios")
 
 const getCiudades = async () => {
     const ciudades = await axios.get("https://infra.datos.gob.ar/catalog/modernizacion/dataset/7/distribution/7.5/download/localidades.json")
+    .catch(error => console.log(error))
     await ciudades.data.localidades.map(m => async function(){
         const ciudadActual = await Ciudad.create({name: m.nombre})
         const provincia = await Provincia.findByPk(m.provincia.id)
