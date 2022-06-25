4c770cae7f89df159157950aaaa1ca3a2e42e1b4
ezefaz <ezefaz@gmail.com>
2022-06-25 16:45:31 -0300

    Features componentes hecho

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import Features from '../Features/Features.jsx';
 import Landing from '../Landing/Landing.jsx';
 import Testimonials from '../Testimonials/Testimonials.jsx';
+
 export function Home() {
   return (
     <div>
       <Landing />
+      <h2>¿En qué aspectos trabaja la psicología?</h2>
+      <Features />
       <h2>Testimonios de nuestros clientes</h2>
       <Testimonials />
+
     </div>
   );
 }
