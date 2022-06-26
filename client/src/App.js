a0af53c269c4d32626c8da75193dbef6834e998c
Miguel <migtovarve@gmail.com>
2022-06-26 10:15:16 -0400

    prueba tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,9 @@ function App() {
 
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+      </h1>
         {
           (status === 'authenticated') 
           ? <Routes>
