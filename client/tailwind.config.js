f7932c14e57f46c6f1108becca35c8e2596a254a
Miguel <migtovarve@gmail.com>
2022-06-26 15:00:57 -0400

    Estilos Log in

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      scale: {
+      '40': '0.40',
+    },
+    colors: {
+      primary: '#575989',
+      secundary: '#f0efff',
+      darkgray: "#212329",
+    },
+  },
   },
   plugins: [],
 }
