644fc653fa864bdbf47163246a005ae4ae87e51b
Alejandro Palacio <alejandrooss94@gmail.com>
2022-06-26 19:06:38 -0500

    funcion de ordenamiento lista

--- client/src/slice/psico/psicologySlice.js
+++ client/src/slice/psico/psicologySlice.js
@@ -5,16 +5,57 @@ export const psicologySlice = createSlice({
     name: 'psicology',
     initialState: {
         psychologists: [],
-        spatiality: []
+        spatiality: [],
+        sortPsycho: [],
+        copyPsycho: [],
     },
     reducers: {
         getPsychos: (state, {payload}) => {
             state.psychologists = payload
+            state.sortPsycho = payload
         },
         filterSpatiality: (state, {payload}) => {
             state.spatiality = payload
-        }
+            state.copyPsycho = payload
+        },
+        sortByNamePsycho: (state, {payload}) => {
+            if(payload === 'asc') {
+                return {
+                    ...state,
+                    psychologists: state.sortPsycho?.slice().sort((a,b) => {
+                        if(a.name > b.name) return 1;
+                        else if (a.name < b.name) return -1;
+                        return 0;
+                    }),
+                    spatiality: state.copyPsycho?.slice().sort((a,b) => {
+                        if(a.name > b.name) return 1;
+                        else if (a.name < b.name) return -1;
+                        return 0;
+                    }),
+                }
+            } else if(payload === 'desc') {
+                return {
+                    ...state,
+                    psychologists: state.sortPsycho?.slice().sort((a,b) => {
+                        if(a.name < b.name) return 1;
+                        else if (a.name > b.name) return -1;
+                        return 0;
+                    }),
+                    spatiality: state.copyPsycho?.slice().sort((a,b) => {
+                        if(a.name < b.name) return 1;
+                        else if (a.name > b.name) return -1;
+                        return 0;
+                    }),
+                }
+            } else {
+                return {
+                    ...state,
+                    psychologists: state.sortPsycho,
+                    spatiality: state.copyPsycho,
+                }
+            }
+        },
     },
 });
 
-export const {getPsychos, filterSpatiality} = psicologySlice.actions; 
+export const {getPsychos, filterSpatiality, sortByNamePsycho} = psicologySlice.actions; 
