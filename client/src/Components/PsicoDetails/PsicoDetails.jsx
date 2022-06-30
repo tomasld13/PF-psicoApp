6e1de873c0ca1be236d888b342962601db31bce3
Miguel <migtovarve@gmail.com>
2022-06-30 09:36:15 -0400

    pricing en details

--- client/src/Components/PsicoDetails/PsicoDetails.jsx
+++ client/src/Components/PsicoDetails/PsicoDetails.jsx
@@ -2,6 +2,7 @@ import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react"; 
 import { getPsychologyID } from "../../slice/psico/thunks";
+import Pricing from "../Pricing/Pricing.jsx"
 
 export default function PsicoDetails() {
 
@@ -15,9 +16,10 @@ export default function PsicoDetails() {
             <div className="container w-1/2 h-60 bg-secundary border border-primary my-5 mx-10">
                 <div>Info</div>
             </div>
-            <div className="container w-1/2">
+
+        <div className="container w-1/2">
                 <div className="container h-80 bg-red">Calendario</div>
-                <div className="container h-80 bg-blue">Servicios</div>
+                <div className="container bg-blue"> <Pricing /> </div>
                 <button className="bg-primary text-white border border-primary font-bold py-2 px-4 rounded hover:bg-white hover:text-primary my-2.5 h-9">Agendar cita</button>
             </div>
         </div>
