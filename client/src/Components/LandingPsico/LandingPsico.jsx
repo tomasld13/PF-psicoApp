b641f3767398eb20c2efd6c124befb4ca2e1bc79
ezefaz <ezefaz@gmail.com>
2022-07-09 15:59:54 -0300

    a

--- client/src/Components/LandingPsico/LandingPsico.jsx
+++ client/src/Components/LandingPsico/LandingPsico.jsx
@@ -14,13 +14,6 @@ export default function LandingPsico() {
           <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
             Fast, flexible financing for
           </p>
-          <Typed
-          className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
-            strings={['BTB', 'BTC', 'SASS']}
-            typeSpeed={120}
-            backSpeed={140}
-            loop
-          />
         </div>
         <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
         <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
