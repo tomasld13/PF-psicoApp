7aaf5e5862343322d1af8a85f8ea684262f75f8f
tomasld13 <tomasld13@gmail.com>
2022-07-10 23:06:12 -0300

    Se borra un console.log

--- api/src/routes/GananciasManager/gananciasRoutes.js
+++ api/src/routes/GananciasManager/gananciasRoutes.js
@@ -2,7 +2,7 @@ const Router = require("express");
 const getGanancias = require("./gananciasController");
 
 const router = Router()
-console.log("hhhh")
+
 router.get('/:fecha', getGanancias);
 
 module.exports = router;
