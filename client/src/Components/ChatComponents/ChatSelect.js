946c5b132ff009afc098ce80efe8bac1ff5e0da9
Pablo Causa <causapablo@gmail.com>
2022-07-12 13:54:17 -0300

    chat funcional

---
+++ client/src/Components/ChatComponents/ChatSelect.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const ChatSelect = () => {
+    return (
+        <div className="middle-screen">
+            <div className="alert-info">
+                <hr />
+                <h3>Elija un psicologo favorito</h3>
+                <span>
+                    Para comenzar una conversación
+                </span>
+
+            </div>
+        </div>
+    )
+}
