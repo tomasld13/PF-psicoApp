6c4c6ee59830e86da1eb7f7ba427c44db1f7db26
ezefaz <ezefaz@gmail.com>
2022-07-12 01:42:10 -0300

    arreglando el responsive

--- client/src/Components/ForPsicos/LandingPsico.jsx
+++ client/src/Components/ForPsicos/LandingPsico.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import background from './background2.jpg'
+import styled from 'styled-components'
 
 const LandingPsico = () => {
     return (
         <div className="text-white">
-          <img src={background} alt='' className=' absolute  -z-10 brightness-500 blur-md' />
-          <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+          <img src={background} alt='' className=' w-full h-full object-cover mix-blend-overlay absolute  -z-10 brightness-500 blur-md' />
+          <div className='max-w-[800px] max-h-[1000px]  mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
             <p className='text-[#575989] font-bold p-2'>
               LOGRA EL CRECIMIENTO QUE QUERES CON NOSOTROS
             </p>
@@ -18,10 +19,18 @@ const LandingPsico = () => {
               </p>
             </div>
             <p className='md:text-2xl text-xl font-bold text-gray-500 text-white'>Mayor visibilidad dentro de nuestra red digital de salud mental.</p>
-            <button className='bg-[#575989] w-[200px] rounded-md font-medium my-6 mx-auto py-3'> <a href='/auth/login'>Comenzar ahora</a> </button>
+            <button className='bg-[#575989] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-white'> <a href='/auth/login'>Comenzar ahora</a> </button>
           </div>
         </div>
       );
 };
 
 export default LandingPsico;
+
+
+// const Imagen = styled.img` 
+
+// @media screen and (max-width: 960px) {
+//   height: 35rem;
+// }
+// `;
