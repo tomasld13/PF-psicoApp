e84315458a6a827e657aa98ccd5f78a7e119b404
Pablo Causa <causapablo@gmail.com>
2022-07-13 13:44:11 -0300

    Cambio en la variable de entorno del SocketContext.js

--- client/src/context/socketContext/SocketContext.js
+++ client/src/context/socketContext/SocketContext.js
@@ -13,7 +13,7 @@ export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
 
-    const { socket, online, conectarSocket, desconectarSocket } = useSocket('http://localhost:3001');
+    const { socket, online, conectarSocket, desconectarSocket } = useSocket(process.env.REACT_APP_API);
     const { auth } = useContext( AuthContext );
     const { dispatch } = useContext( ChatContext );
 
