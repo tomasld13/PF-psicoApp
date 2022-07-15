238f4d70386e0ce1a262301f8aa5c93d2ec33afb
ezefaz <ezefaz@gmail.com>
2022-07-15 14:41:02 -0300

    Cambiando el estilo, todavia falta admin

--- client/src/Components/Features/Features.jsx
+++ client/src/Components/Features/Features.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import FeaturesBox from './FeaturesBox';
-import featureImage from './img/home-05.jpg'
-import featureImage1 from './img/home-06.jpg'
-import featureImage2 from './img/home-07.jpg'
-import featureImage3 from './img/home-08.jpg'
-import featureImage4 from './img/home-09.jpg'
-import featureImage5 from './img/home-10.jpg'
+import featureImage from './img/home-05.png'
+import featureImage1 from './img/home-06.png'
+import featureImage2 from './img/home-07.png'
+import featureImage3 from './img/home-08.png'
+import featureImage4 from './img/home-09.png'
+import featureImage5 from './img/home-10.png'
 
 import './Features.css'
 import SectionTitle from '../SectionTitle/SectionTitle';
